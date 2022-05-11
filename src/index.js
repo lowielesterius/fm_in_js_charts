a4a0e6058310e02ed9f7b07557ca4f9aa64aa142
lowielesterius <lowie.lesterius@gmail.com>
2022-05-11 19:39:31 +0200

    initial set up & chart building & onclick

--- src/index.js
+++ src/index.js
@@ -1,9 +1,87 @@
-//Here we're importing items we'll need. You can add other imports here.
+//import always on top
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+
+//window.functionName > used to trigger the code in Filemaker.
+//function(parameter) > parameter is send via FileMaker as a JSON element
+window.loadChart = function(json){
+
+//this is just to see the result when inspecting the webviewer in FileMaker
+//  console.log(json);
+  const obj = JSON.parse(json);
+//  console.log(obj);
+
+//here we get the data from the parameters passed via FileMaker
+  const type = obj.type;
+  const columns = obj.data;
+  const isRotated = obj.isRotated;
+  const isShowLabels = obj.isShowLabels;
+  const pieLabels = obj.pieLabels;
+  const width = obj.width;
+  const height = obj.height;
+
+  var chart = c3.generate({
+  size: {
+    width: width,
+    height: height
+  },
+  zoom: {
+  enabled: true
+},
+  bindto: '#chart',
+  axis: {
+  x: {
+    type: "category",
+    },
+    rotated: isRotated
+  },
+  data: {
+    onclick: function (clickedData) {
+      console.log(clickedData);
+      const {id, index, value} = clickedData;
+      const month = months[index];
+      const object = {id, month, value};
+      //Code below could replace the 4 lines above
+      //const obj = { id: clickedData.id, month: months[clickedData.index] };
+      console.log(id, index, month);
+      c onsole.log(object);
+      //pass the parameter as JSON.stringify(param)
+      FileMaker.PerformScript("Get Data", JSON.stringify(object));
+    },
+      x: "x",
+      type: type,
+      columns: columns,
+      labels: isShowLabels,
+      colors: {
+                Bananas: 'orange',
+                Kiwi: 'green',
+                Blackberries: 'blue'
+            }
+},
+legend: {
+  position: 'inset',
+  inset: {
+    anchor: 'top-right',
+    x: 20,
+    y: 10,
+    step: 3
+  }
+},
+    
+//bar modifications    
+bar: {
+  width: {
+    ratio: 0.8
+  }  
+},
+//pie modifications    
+pie: {
+  label: {
+    show: pieLabels
+  }
+},
+  });
 };
 
-//
+
